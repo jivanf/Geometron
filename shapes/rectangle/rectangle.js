96cc72a5542b8e3ea676b6f79c7699824962568e
Jivan <minecraftjivan@gmail.com>
2018-03-26 13:07:45 -0700

    Add functionality to rectangle page

--- shapes/rectangle/rectangle.js
+++ shapes/rectangle/rectangle.js
@@ -1,3 +1,51 @@
 /**
  * Created by jivan108 on 3/19/18.
  * */
+window.onload = function() {
+
+
+    let base = document.getElementById("base");
+
+    let height = document.getElementById("height");
+
+    let aResultElement = document.getElementById("area-result");
+    let pResultElement = document.getElementById("perimeter-result");
+
+    base.onkeyup = function () {
+        if (height.value && base.value) {
+
+
+            let b = parseInt(base.value);
+            let h = parseInt(height.value);
+
+            let pResult = 2 * (b + h);
+
+
+            pResultElement.textContent = pResult.toString();
+
+
+            let aResult = b * h;
+
+
+            aResultElement.textContent = aResult.toString();
+        }
+    };
+
+    height.onkeyup = function () {
+        if (height.value && base.value) {
+
+            let b = parseInt(base.value);
+            let h = parseInt(height.value);
+
+            let pResult = 2 * (b + h);
+
+            pResultElement.textContent = pResult.toString();
+
+            let aResult = b * h;
+
+            aResultElement.textContent = aResult.toString();
+        }
+    };
+};
+
+
