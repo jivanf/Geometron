ed1663ecb1981efc9dbb9579a851ced65482ca32
Jivan <minecraftjivan@gmail.com>
2018-03-27 11:27:55 -0700

    Edit circle.js and add functionality to triangle page

--- shapes/circle/circle.js
+++ shapes/circle/circle.js
@@ -1,3 +1,62 @@
 /**
  * Created by jivan108 on 3/26/18.
  */
+window.onload = function() {
+
+
+    let radius = document.getElementById("radius");
+    let diameter = document.getElementById("diameter");
+
+    let aResultElement = document.getElementById("area-result");
+    let cResultElement = document.getElementById("circumference-result");
+
+    radius.onkeyup = function () {
+        if (radius.value) {
+
+
+            let d;
+
+            let cResult;
+
+            if (diameter.value) {
+                d = parseInt(diameter.value);
+                cResult = Math.PI * d;
+                cResultElement.textContent = cResult.toString();
+            }
+
+
+
+            let r = parseInt(radius.value);
+
+
+
+            let aResult = Math.PI * Math.pow(r, 2);
+
+
+            aResultElement.textContent = aResult.toString();
+        }
+    };
+
+    diameter.onkeyup = function () {
+        if (diameter.value) {
+
+            let r;
+            let aResult;
+
+            if (radius.value) {
+                r = parseInt(radius.value);
+                aResult = Math.PI * Math.pow(r, 2);
+                aResultElement.textContent = aResult.toString();
+            }
+            let d = parseInt(diameter.value);
+
+            let cResult = Math.PI * d;
+
+
+            cResultElement.textContent = cResult.toString();
+
+
+
+        }
+    };
+};
--- shapes/triangle/triangle.js
+++ shapes/triangle/triangle.js
@@ -1,3 +1,54 @@
 /**
  * Created by jivan108 on 3/19/18.
  */
+window.onload = function() {
+
+
+    let base = document.getElementById("base");
+
+    let height = document.getElementById("height");
+
+    let aResultElement = document.getElementById("area-result");
+    let pResultElement = document.getElementById("perimeter-result");
+
+    base.onkeyup = function () {
+        if (base.value) {
+
+            let b = parseInt(base.value);
+
+            if (height.value) {
+                let h = parseInt(height.value);
+                let aResult = b * h / 2;
+                aResultElement.textContent = aResult.toString();
+            }
+
+
+            let pResult = 3 * b;
+
+
+            pResultElement.textContent = pResult.toString();
+
+
+
+
+
+
+        }
+    };
+
+    height.onkeyup = function () {
+        if (height.value && base.value) {
+
+            let b = parseInt(base.value);
+            let h = parseInt(height.value);
+
+            let pResult = 3 * b;
+
+            pResultElement.textContent = pResult.toString();
+
+            let aResult = b * h / 2;
+
+            aResultElement.textContent = aResult.toString();
+        }
+    };
+};
